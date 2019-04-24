eb043b537468c1a7defed095fec73e0581021051
Thomas Zarebczan <tzarebczan@users.noreply.github.com>
2019-04-24 08:44:33 -0400

    Adjust download timeouts settings
    Address https://github.com/lbryio/lbry/issues/2016 for timeout issues until we can deploy more reflector peers. Would like @jackrobison to review.

--- lbrynet/conf.py
+++ lbrynet/conf.py
@@ -478,8 +478,8 @@ class Config(CLIConfig):
     )
 
     # protocol timeouts
-    download_timeout = Float("Cumulative timeout for a stream to begin downloading before giving up", 30.0)
-    blob_download_timeout = Float("Timeout to download a blob from a peer", 30.0)
+    download_timeout = Float("Cumulative timeout for a stream to begin downloading before giving up", 90.0)
+    blob_download_timeout = Float("Timeout to download a blob from a peer", 60.0)
     peer_connect_timeout = Float("Timeout to establish a TCP connection to a peer", 2.0)
     node_rpc_timeout = Float("Timeout when making a DHT request", constants.rpc_timeout)
 
